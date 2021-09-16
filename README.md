cc6d56e25f3c61c877a58ea00a0fd7e0ec3d9d9d
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2021-09-16 08:39:00 +0800

    linking with SDL2 and vulkan, tried to load vulkan extensions.

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
     sudo apt install vulkan-tools libvulkan-dev vulkan-validationlayers-dev spirv-tools libglm-dev libsdl2-dev
     mkdir build && cd build && cmake .. && make
-    cd src && ./main
+    cd ./src/base-vulkan-loading && ./BaseProject
 
 
 # References
 
     - https://github.com/google/shaderc/blob/main/downloads.md
     - https://vulkan-tutorial.com/
+    - https://trenki2.github.io/blog/2017/06/02/using-sdl2-with-cmake/
 
 
--- src/base-vulkan-loading/main.cpp
+++ src/base-vulkan-loading/main.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
+#include <vector>
 #include "SDL2/SDL.h" 
+#include "SDL2/SDL_vulkan.h"
+#include "vulkan/vulkan.h"
+#include "vulkan/vulkan_core.h"
 
-int main(int argc, char *argv[])
-{
-    SDL_Init(SDL_INIT_VIDEO);
+
+int main(int argc, char *argv[]) {
+
+    SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window *window = SDL_CreateWindow(
             "Hello Vulkan API with SDL2",
@@ -10,15 +16,21 @@ int main(int argc, char *argv[])
             SDL_WINDOWPOS_UNDEFINED,
             640,
             480,
-            0
+            SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN
             );
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    unsigned int ext_count = 0;
+    SDL_Vulkan_GetInstanceExtensions(window, &ext_count, nullptr);
+
+    std::vector<const char*> ext_names(ext_count);
+    SDL_Vulkan_GetInstanceExtensions(window, &ext_count, ext_names.data());
+
+    std::cout<<"Энэ техник хангамжинд "<<ext_count<<" ширхэг extension байна :"<<std::endl;
+    for (unsigned int i = 0; i < ext_count; i++) {
+        std::cout<<i<<": "<<ext_names[i]<<std::endl;
+    }
+
 
     SDL_DestroyWindow(window);
     SDL_Quit();
