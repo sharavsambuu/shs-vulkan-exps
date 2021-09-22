87d2ea22a996cb843663bf83febaa6409b87afe4
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2021-09-22 23:35:53 +0800

    exp : calling function defined in Lua from C++

--- src/hello-cpp-lua/main.cpp
+++ src/hello-cpp-lua/main.cpp
@@ -29,9 +29,26 @@ int main(int argc, char **argv) {
     // string доторхи Lua код ажиллуулах
     luaL_dostring(L, "print('Hello from Lua')");
     luaL_dostring(L, "for x = 1, 5 do print(x) end");
-
     // файлаас Lua script дуудаж ажиллуулах
     luaL_dofile(L, "hello.lua");
+
+
+    // Lua хэл дээр тоо нэмэх функц тодорхойлох
+    luaL_dostring(L, "function add(x, y) return x+y end");
+    // C++ ээс Lua дээр тодорхойлсон функцыг дуудаж ажиллуулах
+    // global "foo"-ийн утгыг stack-руу хийх
+    lua_getglobal(L, "add");
+    // утга оруулах
+    lua_pushinteger(L, 5);
+    lua_pushinteger(L, 3);
+    // 2 оролт 1 гаралттай функцыг дуудах
+    lua_call(L, 2, 1); 
+    // stack орой дээрхи бүхэл утгыг авах
+    int result = lua_tointeger(L, -1);
+    std::cout << "result of Lua function, 5+3=" << result << std::endl;
+    // stack-ийн эхлэлрүү буцах
+    lua_pop(L, 1);
+
 
     // Lua скрипт хөрвүүлэгчээр үүсгэгдсэн нөөцүүдийг чөлөөлөх
     lua_close(L);
