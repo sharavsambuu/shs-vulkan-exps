de4c828f3009f3ea10350cbdd38fe878b3df658b
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2021-09-26 02:10:30 +0800

    polymorphic components through inheritance interfacing, this mechanism is allowing to cast various components from common memory pointer.

--- src/hello-ecs-lua/hello_flecs_sol.cpp
+++ src/hello-ecs-lua/hello_flecs_sol.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
+#include <string>
 #include <sstream>
 #include <exception>
+#include <typeinfo>
 #include <flecs.h>
 #include "sol/sol.hpp"
+#include "sol/types.hpp"
 
+template<typename T> std::string type_name();
 
 // Component types
-struct Position {
+struct BaseComponent {
+    virtual void foo() {} // polymorphic
+};
+struct Position : BaseComponent {
     float x; 
     float y;
+    Position() {}
+    Position(float px, float py) {
+        x = px;
+        y = py;
+    }
+    void foo() override {}
     std::string to_string() const {
         std::stringstream ss;
         ss<<"Position<x="<<std::to_string(x)<<", y="<<std::to_string(y)<<">";
         return ss.str();
     }
 };
-struct Velocity {
+struct Velocity : BaseComponent {
     float x; 
     float y;
+    Velocity() {}
+    Velocity(float px, float py) {
+        x = px;
+        y = py;
+    }
+    void foo() override {}
     std::string to_string() const {
         std::stringstream ss;
         ss<<"Velocity<x="<<std::to_string(x)<<", y="<<std::to_string(y)<<">";
@@ -45,10 +64,14 @@ int main() {
 
 
     // Зарим нэг entity C++ дээр үүсгэх
-    auto hero = ecs.entity("Hero")
-        .set(Position{2, 4})
-        .set(Velocity{1, 2});
-        //.add<Eats, Fruit>();
+    auto hero = ecs.entity("Hero");
+    Position pos_comp = Position(2, 1);
+    hero.set((Position)pos_comp);
+    Velocity vel_comp = Velocity(4, 1);
+    hero.set((Velocity)vel_comp);
+    hero.add<Eats, Fruit>();
+
+
     std::cout<<hero.name()<<"'s got: ["<<hero.type().str()<<"]"<< std::endl;
     ecs.progress();
     ecs.progress();
@@ -57,37 +80,56 @@ int main() {
 
 
     // Lua хэл дотроос C++ дээр үүсгэгдсэн ecs ажиллаж үзэх
+    lua.new_usertype<BaseComponent>("BaseComponent");
     lua.new_usertype<Position>("Position",
-        sol::call_constructor,
-        sol::factories([](float x, float y) {
-            return Position{x, y};
+        sol::constructors<Position(float, float)>(),
+        sol::call_constructor, sol::factories([](float x, float y) {
+            return Position(x, y);
         }),
         "x", &Position::x,
-        "y", &Position::y
+        "y", &Position::y,
+        sol::base_classes, sol::bases<BaseComponent>()
     );
     lua.new_usertype<Velocity>("Velocity",
-        sol::call_constructor,
-        sol::factories([](float x, float y) {
-            return Velocity{x, y};
+        sol::constructors<Velocity(float, float)>(),
+        sol::call_constructor, sol::factories([](float x, float y) {
+            return Velocity(x, y);
         }),
         "x", &Velocity::x,
-        "y", &Velocity::y
+        "y", &Velocity::y,
+        sol::base_classes, sol::bases<BaseComponent>()
     );
-    lua.new_usertype<Eats>    ("Eats"    );
-    lua.new_usertype<Fruit>   ("Fruit"   );
-    lua.new_usertype<Meat>    ("Meat"    );
+    lua.new_usertype<Eats> ("Eats" );
+    lua.new_usertype<Fruit>("Fruit");
+    lua.new_usertype<Meat> ("Meat" );
+
+    // Lua дээр дуудаж болохоор API маягийн юм бүрдүүлэх
+    lua["ecs_create_entity"] = [](const flecs::world& ecs, const std::string name) {
+        return ecs.entity(name.c_str());
+    };
+    // BaseComponent-ээс удамшуулж интерфэйс үүсгэх байдлаар полиморфизмын дагуу
+    // янз бүрийн төрлийн компонентүүдийг нэг функц ашиглан хаягаар нь хандаж оноох
+    lua["ecs_set_component_to_entity"] = [](flecs::entity& entity, BaseComponent& comp_src) {
+        if (typeid(comp_src)==typeid(Position)) {
+            Position* comp_dest = dynamic_cast<Position*>(&comp_src);
+            entity.set((Position)(*comp_dest));
+        } else 
+        if (typeid(comp_src)==typeid(Velocity)) {
+            Velocity* comp_dest = static_cast<Velocity*>(&comp_src);
+            entity.set((Velocity)(*comp_dest));
+        }
+    };
 
     auto result = lua.script(R"(
-        pos = Position(1., 2.)
+        pos = Position.new(1., 2.)
         vel = Velocity(3., 3.)
         print('inspecting components into Lua')
         print(pos)
         print(vel)
-        --local monster = ecs:entity('Monster'):set(Position.new(2,3))
-        --monster::set(Position(5,6))
-        --monster::set(Velocity(1,1))
-        --monster::add(Eats)
-        --monster::add(Meat)
+
+        monster = ecs_create_entity(ecs, "Monster Chan")
+        ecs_set_component_to_entity(monster, pos)
+        ecs_set_component_to_entity(monster, vel)
         print('created monster entity from Lua')
     )");
     if (!result.valid()) {
@@ -100,6 +142,9 @@ int main() {
     std::cout<<pos_lua.to_string()<<std::endl;
     Velocity& vel_lua = lua["vel"];
     std::cout<<vel_lua.to_string()<<std::endl;
+    flecs::entity& monster = lua["monster"];
+    std::cout<<"monster's name created by Lua script in c++ : "<<monster.name()<<std::endl;
+    std::cout<<monster.name()<<"'s got: ["<<monster.type().str()<<"]"<< std::endl;
 
 
     return 0;
